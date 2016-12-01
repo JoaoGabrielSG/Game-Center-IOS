3c42160e67c8b14a216380c52f8095399c850c47
joaogabrieldosantosgoncalves@gmail.com <joaogabrieldosantosgoncalves@gmail.com>
2016-12-01 11:44:33 -0300

    Creating project

---
+++ Game_center/Game_center/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Game_center
+//
+//  Created by joão gabriel on 30/11/16.
+//  Copyright © 2016 bepid. All rights reserved.
+//
+
+import UIKit
+import GameKit
+
+class ViewController: UIViewController, GKGameCenterControllerDelegate {
+    
+    @IBOutlet weak var lblScore: UILabel!
+    
+    var score: Int = 0 // Stores the score
+    
+    var gcEnabled = Bool() // Stores if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Stores the default leaderboardID
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.authenticateLocalPlayer()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func authenticateLocalPlayer() {
+        let localPlayer: GKLocalPlayer = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if((ViewController) != nil) {
+                // 1 Show login if player is not logged in
+                self.present(ViewController!, animated: true, completion: nil)
+            } else if (localPlayer.isAuthenticated) {
+                // 2 Player is already euthenticated & logged in, load game center
+                self.gcEnabled = true
+                
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: {(leaderboardIdentifer: String?, error: Error?) -> Void in
+                    if error != nil {
+                        print(error)
+                    } else {
+                        self.gcDefaultLeaderBoard = leaderboardIdentifer!
+                    }
+                })
+                
+                
+            } else {
+                // 3 Game center is not enabled on the users device
+                self.gcEnabled = false
+                print("Local player could not be authenticated, disabling game center")
+                print(error)
+            }
+            
+        }
+        
+    }
+    
+    @IBAction func PlusOne(_ sender: UIButton) {
+        score += 1
+        lblScore.text = "\(score)"
+    }
+    
+    @IBAction func minusOne(_ sender: UIButton) {
+        
+        score -= 1
+        lblScore.text = "\(score)"
+    }
+    
+    @IBAction func submitScore(_ sender: UIButton) {
+        
+        let leaderboardID = "Treino_classificacao" //Put here the ID what you put in itunes connect.
+        let sScore = GKScore(leaderboardIdentifier: leaderboardID)
+        sScore.value = Int64(score)
+        
+        GKScore.report([sScore], withCompletionHandler: {(error: Error?) -> Void in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Score submitted")
+                
+            }
+        })
+    }
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func showLeaderboard(_ sender: UIButton) {
+        let gcVC: GKGameCenterViewController = GKGameCenterViewController()
+        gcVC.gameCenterDelegate = self
+        gcVC.viewState = GKGameCenterViewControllerState.leaderboards
+        gcVC.leaderboardIdentifier = "Treino_classificacao"//Put here the ID what you put in itunes connect.
+        self.present(gcVC, animated: true, completion: nil)
+    }
+
+}
+
